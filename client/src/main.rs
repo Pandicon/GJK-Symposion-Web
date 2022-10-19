27cf607a6eb5117108f1472f2b3bbbb094264b03
Pandicon <70060103+Pandicon@users.noreply.github.com>
2022-10-19 21:39:42 +0200

    Improve the CSS handling
    No need for an external library and it also lets us style everything as usual

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,16 +1,12 @@
-use stylist::{yew::styled_component, Style};
 use yew::prelude::*;
 
-const STYLE_FILE: &str = include_str!("main.css");
-
-#[styled_component(App)]
+#[function_component(App)]
 fn app() -> Html {
-    let stylesheet = Style::new(STYLE_FILE).unwrap();
     html! {
-        <div class={stylesheet}>
+        <>
         <h1>{"Nazdárek!"}</h1>
         <h2>{"Ahojky!"}</h2>
-        </div>
+        </>
     }
 }
 
