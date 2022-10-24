bf68dfe0ab11e85fca2e328d441a772a75b909f9
Pandicon <70060103+Pandicon@users.noreply.github.com>
2022-10-24 10:25:30 +0200

    Console log a "MOSTY" text

--- client/src/main.rs
+++ client/src/main.rs
@@ -7,8 +7,11 @@ use crate::router::{switch, Route};
 use yew::prelude::*;
 use yew_router::prelude::*;
 
+const MOSTY_TEXT: &str = include_str!("../mosty_text.txt");
+
 #[function_component(App)]
 fn app() -> Html {
+	gloo::console::log!(MOSTY_TEXT);
 	html! {
 		<BrowserRouter>
 			<Switch<Route> render={Switch::render(switch)} />
