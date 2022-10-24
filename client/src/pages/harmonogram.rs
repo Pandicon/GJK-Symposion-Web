bec14b0135c6be37e9860ffeafb0e489e958dd63
Pandicon <70060103+Pandicon@users.noreply.github.com>
2022-10-24 12:34:56 +0200

    Add a cache with null Ids for fields with no additional information

--- client/src/pages/harmonogram.rs
+++ client/src/pages/harmonogram.rs
@@ -34,7 +34,7 @@ pub fn harmonogram(props: &Props) -> Html {
 			days.push((day, day_cache.unwrap().to_owned().data));
 		} else {
 			let response_raw = r#"
-            {"data":{"harmonogram":[[null,{"lecturer":"","title":"LCH","for_younger":false,"id":"0-1"},{"lecturer":"","title":"Sklep GJK","for_younger":false,"id":"0-2"},{"lecturer":"","title":"Strecha GJK","for_younger":false,"id":"0-3"}],[{"lecturer":"","title":"9:05 - 11:05","for_younger":false,"id":"1-0"},{"lecturer":"<script>alert('cheche!');</script>","title":"<script>alert('hehe!');</script>","for_younger":false,"id":"1-1","row_span":2},null,null],[{"lecturer":"","title":"11:05 - 11:55","for_younger":false,"id":"2-0"},{"lecturer":"prednasejici #1","title":"vysoce narocne tema tykajici se mostu","for_younger":false,"id":"2-2"},null],[{"lecturer":"","title":"12:01 - 13:02","for_younger":false,"id":"3-0"},{"lecturer":"prednasejici #3","title":"odpalování mostů","for_younger":false,"id":"3-1"},{"lecturer":"pan prednasejici #1","title":"symposion web stranky jako most mezi organizatory a ucastniky","for_younger":true,"id":"3-2","row_span":2},{"lecturer":"pani prednasejici #1","title":"rezonance mostu ve vetru","for_younger":true,"id":"3-3","row_span":2}],[{"lecturer":"","title":"13:02 - 23:42","for_younger":false,"id":"4-0"},null],[{"lecturer":"","title":"23:42 - 23:57","for_younger":false,"id":"5-0"},{"lecturer":"","title":"VEČEŘE","for_younger":true,"id":"5-1","col_span":3}],[{"lecturer":"","title":"23:59 - 24:00","for_younger":false,"id":"6-0"},null,null,{"lecturer":"prednasejici #2","title":"pozorování hvězd na téma most","for_younger":false,"id":"6-3"}]],"last_updated":1666602581},"error":null}
+            {"data":{"harmonogram":[[null,{"lecturer":"","title":"LCH","for_younger":false,"id":null},{"lecturer":"","title":"Sklep GJK","for_younger":false,"id":"0-2"},{"lecturer":"","title":"Strecha GJK","for_younger":false,"id":"0-3"}],[{"lecturer":"","title":"9:05 - 11:05","for_younger":false,"id":null},{"lecturer":"<script>alert('cheche!');</script>","title":"<script>alert('hehe!');</script>","for_younger":false,"id":null,"row_span":2},null,null],[{"lecturer":"","title":"11:05 - 11:55","for_younger":false,"id":null},{"lecturer":"prednasejici #1","title":"vysoce narocne tema tykajici se mostu","for_younger":false,"id":"2-2"},null],[{"lecturer":"","title":"12:01 - 13:02","for_younger":false,"id":null},{"lecturer":"prednasejici #3","title":"odpalování mostů","for_younger":false,"id":"3-1"},{"lecturer":"pan prednasejici #1","title":"symposion web stranky jako most mezi organizatory a ucastniky","for_younger":true,"id":"3-2","row_span":2},{"lecturer":"pani prednasejici #1","title":"rezonance mostu ve vetru","for_younger":true,"id":"3-3","row_span":2}],[{"lecturer":"","title":"13:02 - 23:42","for_younger":false,"id":null},null],[{"lecturer":"","title":"23:42 - 23:57","for_younger":false,"id":null},{"lecturer":"","title":"VEČEŘE","for_younger":true,"id":"5-1","col_span":3}],[{"lecturer":"","title":"23:59 - 24:00","for_younger":false,"id":null},null,null,{"lecturer":"prednasejici #2","title":"pozorování hvězd na téma most","for_younger":false,"id":"6-3"}]],"last_updated":1666606780},"error":null}
             "#; // TODO: Make this an actual API call once the API is set up
 			let response = serde_json::from_str::<HarmonogramDayResponse>(response_raw);
 			if let Ok(schedule) = response {
@@ -50,9 +50,6 @@ pub fn harmonogram(props: &Props) -> Html {
 			}
 		}
 	}
-	/*let on_td_click = Callback::from(|_| {
-		gloo::console::log!("Hello!");
-	});*/
 	html! {
 		<>
 		<header>
