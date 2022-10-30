bc629e15e791086741114cd21b87c8aa70e5ba86
Pandicon <70060103+Pandicon@users.noreply.github.com>
2022-10-30 21:20:26 +0100

    Improve the 404 page

--- client/src/pages/not_found.rs
+++ client/src/pages/not_found.rs
@@ -1,3 +1,5 @@
+use crate::{components::link_to::LinkTo, router::Route};
+
 use yew::prelude::*;
 
 /// The #NotFound page
@@ -7,8 +9,35 @@ pub fn not_found() -> Html {
 	yew_hooks::use_title("404 | Mosty - Symposion 2022 | Gymnázium Jana Keplera".to_string());
 	html! {
 		<>
-		<h1>{"404"}</h1>
-		<h2>{"Most do této destinace spadl"}</h2>
+		<header class="uvod_header">
+			<div class="title">
+				<div class="first_line">
+					<h1>
+					<LinkTo path="/" route={Route::Home} link_style="text-decoration: none; color: inherit;" history_style="cursor: pointer;">
+						<span class="most">{"MOSTY"}</span>
+					</LinkTo>
+					</h1>
+					<div class="opakujici_most"></div>
+				</div>
+				<p><span class="most">{"SYMPOSION GYMNÁZIA JANA KEPLERA"}</span></p>
+			</div>
+			<div class={"date"}>
+				<p><span class="most">{"16. - 18. listopadu 2022"}</span></p>
+				<div class="opakujici_most"></div>
+			</div>
+		</header>
+		<main>
+			<div class="opakujici_most"></div>
+			<div class="mosty_jako" style="transform: rotate(-20deg)">
+				<p class="most" style="transform: rotate(-10deg)">
+					<h2 class="most" style="transform: rotate(25deg)">{"404"}</h2>
+					<h3 class="most">{"Most do této destinace spadl"}</h3>
+				</p>
+			</div>
+			<div class="opakujici_most_naopak"></div>
+		</main>
+		<footer>
+		</footer>
 		</>
 	}
 }
