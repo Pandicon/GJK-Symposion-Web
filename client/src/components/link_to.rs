e2f4816fb2f9ba2bd722118e589f49885f748a0b
Pandicon <70060103+Pandicon@users.noreply.github.com>
2022-10-30 10:35:24 +0100

    Add some basic documentation

--- client/src/components/link_to.rs
+++ client/src/components/link_to.rs
@@ -6,15 +6,23 @@ use yew_router::history::History;
 
 #[derive(PartialEq, Properties, Debug)]
 pub struct Props {
+	/// The router route to which to redirect to
 	pub route: Route,
+	/// The path to redirect to in case it is impossible to use the browser history - should usually correspond to the route chosen
 	pub path: String,
+	/// The children of this component
 	pub children: Option<Children>,
+	/// The styles to apply in case it is a normal <a> link
 	pub link_style: Option<String>,
+	/// The styles to apply in case it uses browser history so the <a> tag has no href attribute
 	pub history_style: Option<String>,
 }
 
+/// # The LinkTo component
+/// This component tries to use browser history for redirects if possible. This way the page doesn't reload and doesn't have to fetch all of the uncached files again, which saves load times.
+/// If browser history is available, it uses the route specified to redirect the user, else it uses the path and acts as a normal link.
 #[function_component(LinkTo)]
-pub fn home(props: &Props) -> Html {
+pub fn link_to(props: &Props) -> Html {
 	html! {
 		<>
 		if let Some(history) = yew_router::hooks::use_history() {
